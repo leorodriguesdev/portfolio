a6b46eca2a590b8b283fd8a84402857656109aee
Leonardo Rodrigues <leorodriguesoffice@gmail.com>
2024-07-30 02:04:58 -0300

    new portfolio

---
+++ js/main.js
@@ -0,0 +1,50 @@
+var jobTitles = ['Front-end','Web', 'Mobile'];
+var experienceTitles = ['experiências', 'experiências', 'experiências'];
+var currentJobTitleIndex = 0;
+
+function typeJobTitle(element, text, callback) {
+    var currentText = '';
+    var currentIndex = 0;
+    var space = "&nbsp;&nbsp;";
+
+    var intervalId = setInterval(function() {
+        if (currentIndex < text.length) {
+            currentText += text[currentIndex];
+            element.innerHTML = currentText + space;
+            currentIndex++;
+        } else {
+            clearInterval(intervalId);
+            callback();
+        }
+    }, 100);
+}
+
+function changeJobTitle() {
+    var jobTitleElement = document.getElementById('job-title');
+    var nextJobTitle = jobTitles[currentJobTitleIndex];
+
+    typeJobTitle(jobTitleElement, nextJobTitle, function() {
+        setTimeout(function() {
+            currentJobTitleIndex = (currentJobTitleIndex + 1) % jobTitles.length;
+            changeJobTitle();
+        }, 3000);
+    });
+}
+
+function changeExperienceTitle() {
+    var jobTitleElement = document.getElementById('experience');
+    var nextJobTitle = experienceTitles[currentJobTitleIndex];
+
+    typeJobTitle(jobTitleElement, nextJobTitle, function() {
+        setTimeout(function() {
+            currentJobTitleIndex = (currentJobTitleIndex + 1) % experienceTitles.length;
+            changeJobTitle();
+        }, 3000);
+    });
+}
+
+$(window).on("load", function(){
+    changeJobTitle();
+    changeExperienceTitle();
+ });
+
